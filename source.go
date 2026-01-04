722218ce36cb9118bc347f7e63c4374e4ead304b
suleiman hamza <hamzasuleiman530@gmail.com>
2026-01-04 19:32:54 +0100

    chore: learning variables, conatants and pointers

--- source.go
+++ source.go
@@ -1,14 +1,5 @@
-package main
+// package main
 
-import (
-	"fmt"
-)
-
-func main() {
-	const boilingF, freezingF = 32, 212
-	fmt.Printf("%g°F = %g°C\n", freezingF, fToC(freezingF))
-}
-
-func fToC(f float64) float64 {
-	return (f - 32) * 5 / 9
-}
+// import (
+// 	"fmt"
+// )
