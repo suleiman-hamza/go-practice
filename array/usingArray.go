7bfeaf8cf42008b825d9fe656b05d02f523f820a
suleiman hamza <hamzasuleiman530@gmail.com>
2026-02-09 11:51:58 +0100

    chore: creating multi-dimensional arrays in array package

--- array/usingArray.go
+++ array/usingArray.go
@@ -4,5 +4,9 @@ import "fmt"
 
 func Arr() {
 	anArray := [4]int{1, 2, 3, 4}
-	fmt.Println(anArray)
+	twoD := [5][2]int{{1, 2}, {3, 4}, {5, 6}, {7, 8}, {9, 10}}
+	threeD := [3][2][1]int{{{43}, {76}}, {{78}, {89}}, {{66}, {88}}}
+	fmt.Println("The length of ", anArray, "is", len(anArray))
+	fmt.Println("The First Element of", twoD, "is", twoD[0])
+	fmt.Println("The length of", threeD, "is", len(threeD))
 }
