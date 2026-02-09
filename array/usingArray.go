7312c99f7bbb327e1e05dc9343c59fd458f80b80
suleiman hamza <hamzasuleiman530@gmail.com>
2026-02-09 12:09:43 +0100

    chore: loop through the multi-dimension array

--- array/usingArray.go
+++ array/usingArray.go
@@ -6,7 +6,21 @@ func Arr() {
 	anArray := [4]int{1, 2, 3, 4}
 	twoD := [5][2]int{{1, 2}, {3, 4}, {5, 6}, {7, 8}, {9, 10}}
 	threeD := [3][2][1]int{{{43}, {76}}, {{78}, {89}}, {{66}, {88}}}
+
 	fmt.Println("The length of ", anArray, "is", len(anArray))
 	fmt.Println("The First Element of", twoD, "is", twoD[0])
 	fmt.Println("The length of", threeD, "is", len(threeD))
+
+	for i := 0; i < len(threeD); i++ {
+		v := threeD[i]
+		// fmt.Println(v)
+		for j := 0; j < len(v); j++ {
+			m := v[j]
+			// fmt.Println(m)
+			for k := 0; k < len(m); k++ {
+				fmt.Print(m[k], " ")
+			}
+		}
+		fmt.Println()
+	}
 }
